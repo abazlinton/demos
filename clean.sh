5449db74cf32b2c70507321143b4485b830d13f7
Alex Bazlinton <abazlinton@gmail.com>
2021-02-20 00:22:53 +0000

    stupid hacks to clean old parcel files

---
+++ clean.sh
@@ -0,0 +1,7 @@
+# try and delete files / suppress errors
+rm ./public/*.js 2> /dev/null || true
+rm ./public/*.html 2> /dev/null || true
+rm ./public/*.map 2> /dev/null || true
+rm ./public/*.png 2> /dev/null || true
+
+
